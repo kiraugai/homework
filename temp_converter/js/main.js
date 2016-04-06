ac79269060d67b87eefa93b83b2a938539569ca3
kiraugai <kiraugai@MacBook-Air-van-Kira.local>
2016-04-05 21:34:15 -0700

    Final temp_converter :)

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,21 @@
-
+// when the user clicks on the "fahrenheit_to_celsius" button
+$('#fahrenheit_to_celsius').on('click', function () {
+ 	// grab the value from #temperature AND store in variable fahrenheit
+ 	var fahrenheit = $('#temperature').val();
+ 	// convert that value to a number
+ 	fahrenheit = parseFloat(fahrenheit);
+ 	// convert to celsius by using formula (fahrenheit - 32) / 1.8; AND store in variable celsius
+ 	var celsius = (fahrenheit - 32) / 1.8;
+ 	// display in #result
+ 	$('#result').html(celsius);
+});
+// when the user clicks on the "celsius_to_fahrenheit" button
+ 	// grab the value from #temperature AND store in variable celsius
+ $('#celsius_to_fahrenheit').on('click', function() {
+ 	// convert that value to a number
+ 	var celsius = $('#temperature').val();
+ 	// convert to fahrenheit by using formula 1.8 * celsius + 32; AND store in variable fahrenheit
+ 	var fahrenheit = 1.8 * celsius + 32;
+ 	// display in #result
+ 	$('#result').html(fahrenheit);
+ });
